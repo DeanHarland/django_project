54481637e15e5c3d80e70d4cfd6e2fbb1d076275
DeanHarland <mrdeanharland@gmail.com>
2026-01-15 08:50:44 +0000

    Django initial setup

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4!jn@lvgprz4)gtq#-0b_c@xyh*%z7cs&7su1tg7dve3bw%l6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
